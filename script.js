8edb76b1c35297f29595284e428b0f3878981751
Ellen Watkins <ellen@scripted.org>
2018-03-12 09:36:43 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+$("#joke-1").click(function(){
+    $("#joke-1").text("Nacho Cheese");
+});
+
+$("#joke-2").dblclick(function(){
+    $("#jokeTwo").css("color", "red");
+});
+
+$("#joke-3").click(function(){
+    $("#joke-3").append("It was two tired");
+});
+
+$("#joke4img").hover(function(){
+    $("#joke4img").attr("src", "http://punpedia.org/wp-content/uploads/2016/09/download-2-3.jpg");
+});
+
+
+
+
